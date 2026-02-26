4a3fa82ee78825b12b56642418b82f2dd138fbf2
vDenight <84413087+vDenight@users.noreply.github.com>
2026-02-26 13:09:56 +0100

    wrapped the get_game_introduction into a correct object as intended

--- backend/app/logic/brain/gm_brain_impl.py
+++ backend/app/logic/brain/gm_brain_impl.py
@@ -19,7 +19,7 @@ class GameMasterBrainImpl(GameMasterBrain):
         Generates the initial world-building parameters based on a theme.
         Mocking this for now.
         """
-        return "The village of Oakhaven was promised to be a respite from the mud and blood of the High Road. But as the sun dips below the jagged peaks of the Spine, the long shadows seem to pull at your heels. The air here is too quiet—no crickets chirp, and the locals bar their doors before the light is even gone. You each find yourselves in The Crow’s Nest tavern, not because of the cheap ale, but because it’s the only hearth still burning. As a heavy, rhythmic thudding begins beneath the floorboards, you realize that whatever is haunting this town isn't coming from the woods. It’s already inside'"
+        return GameIntroductionSequence(content="The village of Oakhaven was promised to be a respite from the mud and blood of the High Road. But as the sun dips below the jagged peaks of the Spine, the long shadows seem to pull at your heels. The air here is too quiet—no crickets chirp, and the locals bar their doors before the light is even gone. You each find yourselves in The Crow’s Nest tavern, not because of the cheap ale, but because it’s the only hearth still burning. As a heavy, rhythmic thudding begins beneath the floorboards, you realize that whatever is haunting this town isn't coming from the woods. It’s already inside'")
 
     def provide_scene_setting(self, history: list[Scene], game_state: GameState) \
             -> tuple[SceneDescriptionSequence, list[GameEvent]]:
